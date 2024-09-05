293172d7dc586aed537589ae2c5982f0901a5bf2
Natalia <natalia.casarregio@gmail.com>
2024-09-05 16:18:41 -0300

    fizemos as modificções para termos uma barra de pesquisa

--- src/theme/color.ts
+++ src/theme/color.ts
@@ -4,4 +4,9 @@ export const colors ={
 
     blue: "#4442f0",
     
+    gray_100:"#EBEBEB",
+    gray_200:"#C4C4C4",
+    gray_300:"#8D8D99",
+    gray_400:"#7C7C8A",
+    gray_800:"#161616",
 }
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,5 +1,7 @@
 import { colors } from "./color";
+import { fontFamily } from "./font-family";
 
 export const theme = {
     colors,
+    fontFamily,
 }
